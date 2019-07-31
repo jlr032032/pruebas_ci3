f5af699cdde0870757dce797d1b35fd3c40fd2f4
Jorge <jlr032032@gmail.com>
2019-07-31 11:43:53 -0400

    Incorpora funciones candidatas para el manejo de la carga de archivos

--- application/controllers/CargaArchivos.php
+++ application/controllers/CargaArchivos.php
@@ -7,4 +7,54 @@ class CargaArchivos extends CI_Controller {
 	{
 		$this->load->view('carga-archivos');
 	}
+	
+	public function cargar()
+	{
+		$respuesta = new stdClass();
+		$respuesta->files = $_FILES;
+		$respuesta->post = $_POST;
+		$this->output->set_content_type('application/json')->set_output(
+			json_encode($respuesta)
+		);
+		/*$uploaddir = "uploads/";
+		$uploadfile = $uploaddir . basename( $_FILES['file1']['name']);
+		
+		if(move_uploaded_file($_FILES['file1']['tmp_name'], $uploadfile)) {
+			$this->output->set_content_type('application/json')->set_output(
+				json_encode('{error: {$_FILES["file1"]["error"]}')
+			);
+		}
+		else {
+			$this->output->set_content_type('application/json')->set_output(
+				json_encode('{error: {$_FILES["file1"]["error"]}')
+			);
+		}*/
+	}
+	
+	public function cargar_()
+	{
+		$config['upload_path']   = './uploads/';
+		$config['allowed_types'] = '*';
+		$config['max_size']      = 1000;
+		$config['max_width']     = 10240;
+		$config['max_height']    = 7680;
+
+		log_message('INFO','hola');
+
+		$this->load->library('upload', $config);
+		if (!$this->upload->do_upload('file1')) {
+			$error = array('error' => $this->upload->display_errors());
+			$this->output->set_content_type('application/json')->set_output(
+				json_encode('{message: error}')
+			);
+			/*$this->load->view('upload_form', $error);*/
+		}
+		else {
+			$data = array('upload_data' => $this->upload->data());
+			$this->output->set_content_type('application/json')->set_output(
+				json_encode($data)
+			);
+			/*$this->load->view('upload_success', $data);*/
+		}
+	}
 }
--- assets/js/carga-archivos.js
+++ assets/js/carga-archivos.js
@@ -1,5 +1,22 @@
 $(function() {
-	
-	
 
-})
+	$('#req-btn').click(function(event) {
+		var formData = new FormData();
+		event.preventDefault();
+		formData.set('text', $('#text').val());
+		formData.set('file1', $('#file1')[0].files);
+		formData.set('file2', $('#file2')[0].files);
+		$.ajax({
+			url: 'cargaArchivos/cargar',
+			data: formData,
+			cache: false,
+			contentType: false,
+			processData: false,
+			method: 'POST'
+		})
+		.done(function(data) {
+			console.log(data);
+		});
+	});
+	
+});
